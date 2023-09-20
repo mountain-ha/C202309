5e4fcb1649c78cd28b2322e1e7e09b4cb529c313
mountain-ha <mountain-ha@naver.com>
2023-09-20 16:19:24 +0900

    week3.2 조건문

--- challenge/week3/choiceIf.c
+++ challenge/week3/choiceIf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	int choice;
+	int choice; //변수 생성
 	printf("1. 파일저장\n");
 	printf("2. 저장 없이 닫기\n");
 	printf("3. 종료\n");
-	scanf_s("%d", &choice);
+	scanf_s("%d", &choice); //변수에 정수 입력받기
 
-	if (choice == 1) {
+	if (choice == 1) { 
 		printf("파일을 저장합니다.");
 	}
 	else if (choice == 2) {
@@ -15,7 +15,7 @@ int main() {
 	}else if (choice == 3) {
 		printf("종료합니다.");
 	}
-	else {
+	else {                  //choice가 1,2,3이 아닐때
 		printf("잘못 입력하셨습니다.");
 
 	}
--- challenge/week3/zeroTwoIf.c
+++ challenge/week3/zeroTwoIf.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	int num ;
+	int num ; //변수 생성
 	printf("숫자를 입력하시오:");
-	scanf_s("%d", &num);
+	scanf_s("%d", &num); //변수에 정수 입력받기
 
-	if (num == 0) {
+	if (num == 0) { //num=0일때
 		printf("zero");
 	}
-	else if (num == 1) {
+	else if (num == 1) { //num=1일때
 		printf("one");
 	}
-	else if (num == 2) {
+	else if (num == 2) { //num=2일때
 		printf("two");
 	}
-	else {
+	else {                  //num이 0,1,2가 아닐때
 		printf("not 0 ~ 2");
 
 	}
