1cfa5bb666a43ddb2f3bb4723634f2afb85d01f9
mountain-ha <mountain-ha@naver.com>
2023-09-27 15:53:03 +0900

    Week4.2. 역피라미드 쌓기

---
+++ challenge/week4/pyramidReverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int floor;
+	printf("몇 층을 쌓겠습니까? (5~100)");
+	scanf_s("%d", &floor);
+
+	int i = floor;
+
+	while (i > 0) { //아래의 과정을 floor번 만큼 반복
+		i--;
+		int j = 0;
+
+		while (j < floor - 1 - i) {
+			printf("S", j++); //s를 floor-i 번 출력
+		}
+
+		int k = 0;
+
+		while (k < 2 * i + 1) {
+			printf("*", k++); //*을 2*i-1번 출력
+		}
+
+		printf("\n");
+
+	}
+	return 0;
+}
