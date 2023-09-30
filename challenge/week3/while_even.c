6b6f7504cb416508c0a4a614ec7fbe39e8ef4546
mountain-ha <mountain-ha@naver.com>
2023-09-30 17:50:00 +0900

    issue fix

--- challenge/week3/while_even.c
+++ challenge/week3/while_even.c
@@ -2,10 +2,10 @@
 
 int main() {
 	int i = 0; 
-	while (i < 10) {
-		i = i + 1;
+	while (i < 10) { //i가 10보다 작으면 반복
+		i = i + 1; //증감식
 		if (i % 2 == 0) {
-			continue;
+			continue; //i가 짝수이면 다시 while 부분으로 돌아감
 		}
 		printf("%d Hello World\n",i );
 	}  
